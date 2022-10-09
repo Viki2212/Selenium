9286441a6ce353e87c7947c85c2f75df4aefbc15
Viki2212 <viigneshsekar@gmail.com>
2022-10-10 01:58:01 +0530

    10/10/2022

--- learnSelenium/Test.java
+++ learnSelenium/Test.java
@@ -1,20 +1,7 @@
 package learnSelenium;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Test {
+
 	
-	public static void main(String[] args) {
-		
-		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
-		
-		driver.get("https://www.facebook.com/");
-		
-		driver.findElementById("email").sendKeys("vskviicky@hotmail.com");
-		driver.findElementById("pass").sendKeys("Viki1466*");
-		driver.findElementByName("login").click();
-		
-						
-	}
+
 }
