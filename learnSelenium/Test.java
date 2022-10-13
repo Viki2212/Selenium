ddd297f2df43c8abfd5cebe828409c6c3a7e5e5b
Viki2212 <viigneshsekar@gmail.com>
2022-10-14 03:00:08 +0530

    14.10.2022

--- learnSelenium/Test.java
+++ learnSelenium/Test.java
@@ -1,12 +1,24 @@
 package learnSelenium;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class Test {
 
 	public static void main(String[] args) {
-
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
 		
+		driver.manage().window().maximize();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		
+	
 	}
 }
-	
-	
-	
+
+
+
